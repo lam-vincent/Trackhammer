dd3531e6fa20086fde642a5d69a7d4c3ab1ba3ab
Vincent <mrccniv@gmail.com>
2023-02-20 00:01:00 +0100

    feat: Eternal War and Maëlstrom pages layout

--- src/pages/Maelstrom.jsx
+++ src/pages/Maelstrom.jsx
@@ -1,19 +1,21 @@
-import {Link} from "react-scroll";
 
-const Home = () => {
+const Maelstrom = () => {
 
   return (
-    <div className="flex-col items-center justify-center px-8">
-      <div className="">
-        <div className="flex flex-col justify-center h-screen bg-cover bg-center p-8 rounded-3xl" style={{ backgroundImage: `url(src/assets/home-page-background.png)` }}>
-          <div className='flex flex-col items-center backdrop-blur-md rounded-3xl p-4 mb-32'>
-          <h1 className="text-5xl font-bold text-white mb-10">Welcome to Maëlstrom</h1>
-          <p className="text-lg text-white mb-10">TrackHammer is an online application for wargamers that helps them keep track of their scores during tabletop battles. The app has three different modes to choose from: you can join the ongoing war in "Eternal War", or start your own custom crusade with friends in "Maëlstrom" and "The TaleMaker". The app is designed to make it easier for wargamers to track their progress and stay engaged in the game they love.</p>
-            <Link to="carousel" spy={true} smooth={true} duration={1750}>
-              <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                  Explore
-              </button> 
-            </Link>
+    <div className="flex-col items-center justify-center px-8 text-white">
+      <div className="flex flex-col justify-center h-screen bg-cover bg-center p-8 rounded-3xl space-y-8" style={{ backgroundImage: `url(src/assets/home-page-background.png)` }}>
+        <div name="title" className="flex items-center justify-center text-4xl font-bold">
+          currently in a private room
+        </div>
+        <div name="code" className="flex items-center justify-center text-xl">
+          code: 9HFMN
+        </div>
+        <div name="container" className="flex items-center justify-center space-x-8">
+          <div name="board" className="flex items-center justify-center border-2 rounded-3xl w-2/3 h-96">
+            board
+          </div>
+          <div name="tabs" className="flex items-center justify-center border-2 rounded-3xl w-1/3 h-96">
+            tabs
           </div>
         </div>
       </div>
@@ -21,4 +23,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Maelstrom;
